8f0706807e240420f6120d4888e8767fc4709387
kirtanchandak <themarketingkirtan@gmail.com>
2022-12-02 23:28:05 +0530

    basic-auth-ver1

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+}
+export default MyApp;
